9c7afea9d7a16954418b11b8d5b4cea550e1df16
Denis Proskurin <4201995+dpvpro@users.noreply.github.com>
2026-03-09 23:53:46 +0300

    fix js linter errors

--- frontend/src/components/file-list-data-provider.ts
+++ frontend/src/components/file-list-data-provider.ts
@@ -122,7 +122,7 @@ export class FileListDataProvider {
           return (sortOrder.direction === "asc") ? 1 : -1;
         }
         // and do a "natural" sort on names
-        const options = { numeric: true, sensitivity: "base" };
+        const options: Intl.CollatorOptions = { numeric: true, sensitivity: "base" };
         if (sortOrder.direction === 'asc') {
           return a.name.localeCompare(b.name, undefined, options);
         } else { 
